54d12fdfc3adbc2246431a5521e5b3410a60ae23
Yenal Yilmaz <109667211+yenalyilmaz@users.noreply.github.com>
2025-07-28 11:36:52 +0300

    Initial Create

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+require("config.cmp")
+
+vim.o.relativenumber = true
+vim.cmd("colorscheme gruvbox")
---
+++ lua/config/lazy.lua
@@ -0,0 +1,27 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Temel eklentiler
+  { "tpope/vim-sensible" },
+  { "preservim/nerdtree" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+  -- LSP ve Otomatik Tamamlama
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/cmp-vsnip" },
+  { "hrsh7th/vim-vsnip" },
+})
+
